aa3083779102c7832b1a3224e5acfc40b32f28c7
Jorge Raul Ochoa-Reyes <jorgeochoareyes@gmail.com>
2025-03-31 19:37:49 -0700

    page paths

--- src/components/layout/DashboardLayout.tsx
+++ src/components/layout/DashboardLayout.tsx
@@ -1,12 +1,14 @@
 import { SidebarProvider, SidebarInset } from "~/components/ui/sidebar";
 import { AppSidebar } from "~/components/sidebar/app-sidebar"; 
+import { PagePath } from "../path/page-path";
 
 export const DashboardLayout: React.FC<{
     children: React.ReactNode;
 }> = ({ children }) => {
   return <SidebarProvider>
     <AppSidebar />
-    <SidebarInset>
+    <SidebarInset> 
+      <PagePath />
       {children}
     </SidebarInset>
   </SidebarProvider>;
