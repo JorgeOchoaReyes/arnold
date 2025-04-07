2874db8b868da9571d97f6f873fc209e883007b6
Jorge Raul Ochoa-Reyes <jorgeochoareyes@gmail.com>
2025-04-06 22:09:12 -0700

    interview basic

---
+++ src/pages/dashboard/interviews/[id].tsx
@@ -0,0 +1,10 @@
+import React from "react"; 
+
+export const InterviewPage = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold text-center mt-10">Interview Page</h1>
+
+    </div>
+  );
+};
